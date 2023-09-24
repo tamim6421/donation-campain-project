44aa9f405b34bf52b354ef4ba78ab41ef410d835
ProgrammingHero <tamimhossain6421@gmail.com>
2023-09-24 19:53:26 +0600

    setup a project environment

---
+++ src/Router/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+   {
+    path:'/',
+    element: <div> hello </div>
+   }
+])
+
+export default router;
